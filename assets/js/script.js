28ad9b646ed939304fdb279673337fdc8c9510c4
Flamy <27062841+f1amy@users.noreply.github.com>
2018-10-26 17:23:38 +0500

    minor changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
 document.getElementById("run-button").onclick = function() {
-  document.getElementById('result-header-text').style.display = 'none';
-  document.getElementById("result-iframe").srcdoc = `<!DOCTYPE html><html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>result</title><style>body{background-color:#fff;}${
+  document.getElementById("result-header-text").style.display = "none";
+  const markup = `<!DOCTYPE html><html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>result</title><style>body{background-color:#fff;}${
     document.getElementById("css-code-area").value
   }</style><script>window.onload=function(){${
     document.getElementById("js-code-area").value
   }};</script></head><body>${
     document.getElementById("html-code-area").value
   }</body></html>`;
+  let iframeLink = document.getElementById("result-iframe");
+  iframeLink.src = `data:text/html;charset=utf-8,${encodeURI(markup)}`;
+  iframeLink.srcdoc = markup;
+  //?
   // another way -> iframeLink.src = 'data:text/html;charset=utf-8,' + encodeURI(htmlCode);
 };
 
